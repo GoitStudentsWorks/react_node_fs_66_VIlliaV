b296cc55f917eac806b31f651ee73af8dd82cb7c
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-07-24 15:00:15 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/final_project_utf-8_front">
+        <BrowserRouter basename="/react_node_fs_66_VIlliaV">
           <App />
         </BrowserRouter>
       </PersistGate>
